45e2d0af5195c3df9a48586ab3844e0a0d95a113
konradrudnicki <266910@student.pwr.edu.pl>
2024-10-24 22:04:10 +0200

    cors config

--- gateway/src/main/java/com/eduvy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/eduvy/gateway/GatewayApplication.java
@@ -2,10 +2,52 @@ package com.eduvy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.server.WebFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://88.198.122.46:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public WebFilter corsWebFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-resources/**", "/register", "/login", "/swagger-ui.html/**")
+                        .permitAll()
+                        .anyExchange().authenticated())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .build();
     }
 }
