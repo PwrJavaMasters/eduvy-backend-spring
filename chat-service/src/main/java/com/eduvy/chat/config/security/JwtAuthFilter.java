8af31e3e70488b8f1ffea6c0b633e988df781501
Filip Szydłak <266912@student.pwr.edu.pl>
2024-11-15 15:14:31 +0100

    add security layer with utils

---
+++ chat-service/src/main/java/com/eduvy/chat/config/security/JwtAuthFilter.java
@@ -0,0 +1,119 @@
+package com.eduvy.chat.config.security;
+
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.JwkProviderBuilder;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@Component
+@Getter
+@Setter
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getJwtFromRequest(request);
+
+        if (token != null && validateToken(token)) {
+            DecodedJWT decodedJWT = decodeToken(token);
+
+            String auth0UserId = decodedJWT.getSubject();
+            List<String> roles = decodedJWT.getClaim("user_roles").asList(String.class);
+            String nickname = decodedJWT.getClaim("nickname").asString();
+            String email = decodedJWT.getClaim("email").asString();
+
+            if (roles == null) {
+                roles = Collections.singletonList("ROLE_USER");
+            }
+
+
+
+            List<GrantedAuthority> authorities = roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            UserDetails userDetails = new UserInfoDetails(auth0UserId, email, nickname, "", authorities);
+
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private JwkProvider jwkProvider;
+
+    private final Auth0Properties auth0Properties;
+
+    public JwtAuthFilter(Auth0Properties auth0Properties) {
+        this.auth0Properties = auth0Properties;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.jwkProvider = new JwkProviderBuilder(auth0Properties.getIssuer())
+                .cached(10, 24, TimeUnit.HOURS) // Cache up to 10 public keys for 24 hours
+                .build();
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            Jwk jwk = jwkProvider.get(jwt.getKeyId());
+            RSAPublicKey publicKey = (RSAPublicKey) jwk.getPublicKey();
+
+            Algorithm algorithm = Algorithm.RSA256(publicKey, null);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(auth0Properties.getIssuer())
+                    .withAudience(auth0Properties.getAudience())
+                    .build();
+
+            verifier.verify(token);
+            return true;
+        } catch (Exception e) {
+            // Log the exception
+            logger.error("JWT validation failed", e);
+            return false;
+        }
+    }
+
+    private DecodedJWT decodeToken(String token) {
+        return JWT.decode(token);
+    }
+}
---
+++ chat-service/src/main/java/com/eduvy/chat/config/security/UserInfoDetails.java
@@ -0,0 +1,71 @@
+package com.eduvy.chat.config.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+public class UserInfoDetails implements UserDetails {
+
+    private String auth0UserId;
+    private String email;
+    private String nickname;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(String auth0UserId, String email, String nickname, String password, List<GrantedAuthority> authorities) {
+        this.auth0UserId = auth0UserId;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.authorities = Collections.unmodifiableList(authorities);
+    }
+
+    public String getAuth0UserId() { return auth0UserId; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
