193303cd06d8bfd1a1996433b55894f815ebc465
konradrudnicki <266910@student.pwr.edu.pl>
2024-11-25 22:44:48 +0100

    payment service

--- payment-service/src/main/java/com/eduvy/payment/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/eduvy/payment/services/impl/PaymentServiceImpl.java
@@ -3,7 +3,6 @@ package com.eduvy.payment.services.impl;
 import com.eduvy.payment.dto.OrderRequest;
 import com.eduvy.payment.services.PayUService;
 import com.eduvy.payment.services.PaymentService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -12,23 +11,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-    @Value("${payu.pos_id}")
-    private String clientId;
+//    @Value("${payu.pos_id}")
+    private String clientId = "300746";
 
-    @Value("${payu.order_create_endpoint}")
-    private String orderCreateEndpoint;
+//    @Value("${payu.order_create_endpoint}")
+    private String orderCreateEndpoint = "https://secure.snd.payu.com/api/v2_1/orders";
 
     @Autowired
     PayUService payUService;
 
+    public PaymentServiceImpl(PayUService payUService) {
+        this.payUService = payUService;
+    }
 
     @Override
     public String createOrder(OrderRequest orderRequest) {
@@ -41,7 +40,7 @@ public class PaymentServiceImpl implements PaymentService {
         headers.add("Content-Type", "application/json");
 
         Map<String, Object> payload = new HashMap<>();
-        payload.put("notifyUrl", "http://twoja_domena.pl/api/payment/notify");
+        payload.put("notifyUrl", "https://eduvy.pl/api/payment/notify");
         payload.put("customerIp", "127.0.0.1");
         payload.put("merchantPosId", clientId);
         payload.put("description", "Zapłata za spotkanie");
@@ -65,6 +64,16 @@ public class PaymentServiceImpl implements PaymentService {
         String redirectUri = ((Map<String, String>) response.getBody().get("redirectUri")).get("redirectUri");
 
         return redirectUri;
+    }
+
+
+    public static void main(String[] args) {
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.setExtOrderId("1");
+
+        PayUService payUService = new PayUServiceImpl();
+        PaymentService paymentService = new PaymentServiceImpl(payUService);
+        System.out.printf(paymentService.createOrder(orderRequest));
     }
 
 }
