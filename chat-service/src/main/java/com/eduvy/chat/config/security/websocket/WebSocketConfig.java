955718f8435a932e93b6d1479af98b0ac7a92ffb
Wojciech Korys <wojtek.korys@wp.pl>
2024-12-05 13:29:21 +0100

    changed websocket config for frontend

--- chat-service/src/main/java/com/eduvy/chat/config/security/websocket/WebSocketConfig.java
+++ chat-service/src/main/java/com/eduvy/chat/config/security/websocket/WebSocketConfig.java
@@ -1,16 +1,22 @@
 package com.eduvy.chat.config.security.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -28,8 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("localhost:8081")
-                .withSockJS();
+                .setAllowedOrigins("http://localhost:4200");
     }
 
     @Override
